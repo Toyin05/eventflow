7226b9838efcac5c8dd353543b3923728e9775c9
Toyin <oluwatoyinojumoro@gmail.com>
2026-04-27 13:54:59 +0100

    more fixes for the routing phase

--- event-ticket/src/main.tsx
+++ event-ticket/src/main.tsx
@@ -1,10 +1,9 @@
 import { createRoot } from 'react-dom/client'
-import { StartClient } from '@tanstack/react-start'
-import { createRouter } from '@tanstack/react-router'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 
 const router = createRouter({ routeTree })
 
 const root = createRoot(document.getElementById('root')!)
 
-root.render(<StartClient router={router} />)
+root.render(<RouterProvider router={router} />)
