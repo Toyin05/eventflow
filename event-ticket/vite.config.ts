78079937b43d36193f73825ba01cc463c765e53d
Toyin <oluwatoyinojumoro@gmail.com>
2026-04-27 13:48:28 +0100

    more fixes...

--- event-ticket/vite.config.ts
+++ event-ticket/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [TanStackRouterVite(), react(), nodePolyfills()],
   base: '/',
   build: {
     outDir: 'dist',
@@ -14,5 +15,8 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  ssr: {
+    noExternal: ['@tanstack/start-storage-context']
   },
 })
